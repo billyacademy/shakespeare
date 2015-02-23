5975b1eb024d56db35609ff75132dfd4eb2267c6
William Mahoney <wm.j.mahoney@gmail.com>
2015-02-23 14:22:24 -0500

    updated controller with helper to allow for sorting columns, no ajax yet

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,6 +1,44 @@
 class RolesController < ApplicationController
   def index
     @play = Play.find(params[:play_id])
-    @roles = Role.all
+    #@roles = Role.order(params[:sort])
+    @roles = sorted_roles(params)
+  end
+
+
+
+
+  private
+  def sort_column
+    Role.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def sorted_roles(params)
+    if params[:type] == 'calculated'
+      if sort_direction == 'asc'
+        return Role.all.sort_by { |role| role.send(params[:sort].to_sym) }
+      else
+        return Role.all.sort_by { |role| role.send(params[:sort].to_sym) }.reverse
+      end
+    else
+      Role.order(sort_column + ' ' + sort_direction)
+    end
   end
 end
+
+#
+# def sorted_foos(params)
+#   @foo = Model.includes(:bar).where(bar: { user_id: current_user.id })
+#   if params[:type] == ‘calculated’
+#     if sort_direction == ‘asc’
+#       @foo.sort_by { |a| a.send(params[:sort]) }
+#     else
+#       @foo.sort_by { |a| a.send(params[:sort]) }.reverse
+#     end
+#   else
+#     @foo.order(sort_column + ” ” + sort_direction)
+#   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def sortable(column, type = nil, title = nil)
+    title ||= column.titleize
+    if column == params[:sort] && params[:direction] == "asc"
+      direction = "desc"
+    else
+      direction = "asc"
+    end
+    # direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
+    link_to title, sort: column, direction: direction, type: type
+  end
 end
