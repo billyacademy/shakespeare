5b1bb3296f8becc7670a964fc5fc171d7f6717c6
William Mahoney <wm.j.mahoney@gmail.com>
2015-02-23 14:21:46 -0500

    added methods to clean up view

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,13 +1,36 @@
 class Role < ActiveRecord::Base
+  belongs_to :play
+  has_many :speeches
+  has_many :lines, through: :speeches
+  has_many :scenes, through: :roles_scenes
+  has_many :roles_scenes
+
   validates :name,
     presence: true
 
   validates :play_id,
     presence: true
 
-  belongs_to :play
-  has_many :speeches
-  has_many :lines, through: :speeches
-  has_many :scenes, through: :roles_scenes
-  has_many :roles_scenes
+  def num_of_scenes
+    scenes.count
+  end
+
+  def percent_of_scenes
+    scenes.count.to_f / Scene.all.count*100
+  end
+
+  def longest_speech
+     max_lines = 0
+     speeches.each do |speech|
+       if speech.lines.count > max_lines
+         max_lines = speech.lines.count
+       end
+      end
+      max_lines
+  end
+
+  def num_of_lines
+    lines.count
+  end
+
 end
