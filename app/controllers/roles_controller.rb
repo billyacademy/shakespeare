9c4a6f0a36f8050705bd10781a0e5c102ac3dfc2
William Mahoney <wm.j.mahoney@gmail.com>
2015-02-23 18:24:14 -0500

    cleaned up roles controller, pagination added

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,12 +1,9 @@
 class RolesController < ApplicationController
+  require 'will_paginate/array'
   def index
     @play = Play.find(params[:play_id])
-    #@roles = Role.order(params[:sort])
     @roles = sorted_roles(params)
   end
-
-
-
 
   private
   def sort_column
@@ -14,31 +11,18 @@ class RolesController < ApplicationController
   end
 
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    ['asc', 'desc'].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
   def sorted_roles(params)
     if params[:type] == 'calculated'
       if sort_direction == 'asc'
-        return Role.all.sort_by { |role| role.send(params[:sort].to_sym) }
+        return Role.all.sort_by { |role| role.send(params[:sort].to_sym) }.paginate(per_page: 10, page: params[:page])
       else
-        return Role.all.sort_by { |role| role.send(params[:sort].to_sym) }.reverse
+        return Role.all.sort_by { |role| role.send(params[:sort].to_sym) }.reverse.paginate(per_page: 10, page: params[:page])
       end
     else
-      Role.order(sort_column + ' ' + sort_direction)
+      Role.order(sort_column + ' ' + sort_direction).paginate(per_page: 10, page: params[:page])
     end
   end
 end
-
-#
-# def sorted_foos(params)
-#   @foo = Model.includes(:bar).where(bar: { user_id: current_user.id })
-#   if params[:type] == ‘calculated’
-#     if sort_direction == ‘asc’
-#       @foo.sort_by { |a| a.send(params[:sort]) }
-#     else
-#       @foo.sort_by { |a| a.send(params[:sort]) }.reverse
-#     end
-#   else
-#     @foo.order(sort_column + ” ” + sort_direction)
-#   end
