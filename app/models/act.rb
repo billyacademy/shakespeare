5b8098bd1b67b7eb6d5830ff2dc2ba707fb88411
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-14 21:41:50 -0400

    added class method parse all to create acts

--- app/models/act.rb
+++ app/models/act.rb
@@ -5,4 +5,7 @@ class Act < ActiveRecord::Base
   belongs_to :play
   has_many :scenes
 
+  def self.parse_all(acts, play)
+    acts.map { |act| Act.create(title: (act).xpath('TITLE').text, play_id: play.id) }
+  end
 end
