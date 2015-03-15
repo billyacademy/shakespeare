3524f2d9fbbd9ddee74341e7b97bab037a3af70c
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-14 21:42:28 -0400

    added a parse and create play method

--- app/models/play.rb
+++ app/models/play.rb
@@ -3,4 +3,12 @@ class Play < ActiveRecord::Base
     presence: true
   has_many :acts
   has_many :roles
+
+  def self.parse(doc)
+    doc.css("PLAY TITLE").children.first.text
+  end
+
+  def self.create_play(title)
+    Play.new(title: title)
+  end
 end
