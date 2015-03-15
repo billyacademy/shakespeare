af88c15b978eb962e6d26c4736f7fdb829a9b08c
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-14 21:43:48 -0400

    added parse all method for creating scenes

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -8,4 +8,8 @@ class Scene < ActiveRecord::Base
   belongs_to :act
   has_many :speeches
   has_many :roles, through: :roles_scenes
+
+  def self.parse_all(scenes)
+    scenes.map { |scene| Scene.create(title: scene.xpath('TITLE').text.gsub("\n", '').strip, act_id: Act.find_by(title: scene.parent.xpath('TITLE').text).id)}
+  end
 end
