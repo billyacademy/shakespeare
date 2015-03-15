ce1bf5e465c47c38946533e6f54e1d34c5327479
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-14 21:44:37 -0400

    added parse all method to create instances of RolesScene

--- app/models/roles_scene.rb
+++ app/models/roles_scene.rb
@@ -7,4 +7,16 @@ class RolesScene < ActiveRecord::Base
 
   validates :scene_id,
     presence: true
+
+  def self.parse_all(speakers)
+    speakers.map do |speaker|
+      unless speaker.text == "All"
+        role_scene_criteria = RolesScene.find_by(scene_id: Scene.find_by(title: speaker.parent.parent.children.first.text.gsub("\n", '').strip).id, role_id: Role.find_by(name: speaker.text).id)
+        if role_scene_criteria == nil
+          RolesScene.create(scene_id: Scene.find_by(title: speaker.parent.parent.children.first.text.gsub("\n", '').strip).id, role_id: Role.find_by(name: speaker.text).id)
+        end
+      end
+    end
+  end
+
 end
