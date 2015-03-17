4c47022e895959381e471a39de49e9b286f57c32
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-17 17:06:34 -0400

    created a helper to clean up nested iterations, will continue to explore ways to make it better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
     end
     link_to title, sort: column, direction: direction, type: type
   end
+
+  def create_line(data, role_info, scene_info)
+    if role_info != nil
+      speech = Speech.create(role_id: role_info.id, scene_id: scene_info.id)
+      data.xpath('LINE').each do |line|
+        Line.create(message: line.children.text.gsub("\n", '').strip, speech_id: speech.id)
+      end
+    end
+  end
 end
--- app/models/speech.rb
+++ app/models/speech.rb
@@ -9,16 +9,13 @@ class Speech < ActiveRecord::Base
   validates :role_id,
     presence: true
 
+  extend ApplicationHelper
+
   def self.parse_all(speeches)
     speeches.map do |speech|
       role = Role.find_by(name: speech.xpath('SPEAKER').first.text)
       scene = Scene.find_by(title: speech.parent.xpath("TITLE").text.gsub("\n", '').strip)
-      if role != nil
-        new_speech = Speech.create(role_id: role.id, scene_id: scene.id)
-        speech.xpath('LINE').each do |line|
-          Line.create(message: line.children.text.gsub("\n", '').strip, speech_id: new_speech.id)
-        end
-      end
+      create_line(speech, role, scene)
     end
   end
 end
