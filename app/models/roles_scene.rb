de1ddc2eecc2fb823c3992ff5b65849e159690a5
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-18 19:57:12 -0400

    cleaned up some code with .try method and stronger/simpler logic

--- app/models/roles_scene.rb
+++ app/models/roles_scene.rb
@@ -10,13 +10,10 @@ class RolesScene < ActiveRecord::Base
 
   def self.parse_all(speakers)
     speakers.map do |speaker|
-      unless speaker.text == "All"
-        role_scene_criteria = RolesScene.find_by(scene_id: Scene.find_by(title: speaker.parent.parent.children.first.text.gsub("\n", '').strip).id, role_id: Role.find_by(name: speaker.text).id)
-        if role_scene_criteria == nil
-          RolesScene.create(scene_id: Scene.find_by(title: speaker.parent.parent.children.first.text.gsub("\n", '').strip).id, role_id: Role.find_by(name: speaker.text).id)
-        end
+      role_scene_criteria = RolesScene.find_by(scene_id: Scene.find_by(title: speaker.parent.parent.children.first.text.gsub("\n", '').strip).id, role_id: Role.find_by(name: speaker.text).try(:id))
+      if role_scene_criteria == nil && speaker.text != "All"
+        RolesScene.create(scene_id: Scene.find_by(title: speaker.parent.parent.children.first.text.gsub("\n", '').strip).id, role_id: Role.find_by(name: speaker.text).id)
       end
     end
   end
-
 end
